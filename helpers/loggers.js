20ce3fce2ff58c732d252f16d67368cb84394a17
Gabriel Kuettel <gabrielkuettel@gmail.com>
2018-11-03 12:40:02 -0600

    update helpers

--- helpers/loggers.js
+++ helpers/loggers.js
@@ -4,21 +4,21 @@ const util = require('util');
 module.exports = {
    userLogger: (req, res, next) => {
       let data = JSON.stringify(req.user);
-      fs.writeFile('./helpers/log/userLog.txt', data, (err) => {
+      fs.writeFile('./helpers/log/user.txt', data, (err) => {
          if (err) throw err;
       });
       next();
    },
    reqLogger: (req, res, next) => {
       let data = util.inspect(req);
-      fs.writeFile('./helpers/log/requestLog.txt', data, (err) => {
+      fs.writeFile('./helpers/log/request.txt', data, (err) => {
          if (err) throw err;
       });
       next();
    },
    resLogger: (req, res, next) => {
       let data = util.inspect(res);
-      fs.writeFile('./helpers/log/responseLog.txt', data, (err) => {
+      fs.writeFile('./helpers/log/response.txt', data, (err) => {
          if (err) throw err;
       });
       next();
