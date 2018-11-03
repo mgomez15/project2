815d6b6822926ebd26afa63050238b86a7238cf3
Gabriel Kuettel <gabrielkuettel@gmail.com>
2018-11-03 13:42:50 -0600

    add logging and overrides

--- helpers/loggers.js
+++ helpers/loggers.js
@@ -4,9 +4,18 @@ const util = require('util');
 module.exports = {
    userLogger: (req, res, next) => {
       let data = JSON.stringify(req.user);
-      fs.writeFile('./helpers/log/user.txt', data, (err) => {
-         if (err) throw err;
-      });
+      if (req.user) {
+         console.log(`User logged in as ${req.user.email}`);
+         fs.writeFile('./helpers/log/user.txt', data, (err) => {
+            if (err) throw err;
+         });
+      } else {
+         console.log('User not logged in.');
+         fs.writeFile('./helpers/log/user.txt', 'User not logged in.', (err) => {
+            if (err) throw err;
+         });
+      }
+      
       next();
    },
    reqLogger: (req, res, next) => {
