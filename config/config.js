15d596c07c20dbf05bb9c09a8681f22d66b45d31
Maria Gomez <mafer_15_s@hotmail.com>
2018-11-07 16:21:06 -0700

    her

--- config/config.js
+++ config/config.js
@@ -1,26 +1,23 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-	development: {
-		database: process.env.DB_NAME,
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		host: process.env.DB_HOST,
-		dialect: "mysql",
-		operatorsAliases: false
+	"development": {
+		"database": "process.env.DB_NAME",
+		"username": "process.env.DB_USERNAME",
+		"password": "process.env.DB_PASSWORD",
+		"host": "process.env.DB_HOST",
+		"dialect": "mysql",
+		"operatorsAliases": "false"
 	},
-	test: {
-		username: "",
-		password: null,
-		database: "",
-		host: "",
-		dialect: "mysql"
+	"test": {
+		"username": "",
+		"password": "null",
+		"database": "",
+		"host": "",
+		"dialect": "mysql"
 	},
-	production: {
-		username: "",
-		password: null,
-		database: "",
-		host: "127.0.0.1",
-		dialect: "mysql"
+	"production": {
+		"use_env_variable": "JAWSDB_URL",
+		"dialect": "mysql"
 	}
 }
